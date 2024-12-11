a58aa1a847a5d95a413becd25f59d20ba84ee3ac
anton-otroshchenko <anton3447@gmail.com>
2024-12-11 10:25:16 +0200

    small fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Дозволити конкретний домен
+    origin: ['http://localhost:3000', 'https://next-nest-graphql-test.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
